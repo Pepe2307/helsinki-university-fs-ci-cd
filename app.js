cdc23ebd754acfa9d6f213775921075040e00f66
unknown <pablopaez2307@gmail.com>
2026-07-07 12:41:37 -0300

    test breaking health 3

--- app.js
+++ app.js
@@ -5,19 +5,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-// El endpoint de salud detecta si está en Render o en tu PC local
+// Endpoint inteligente para simular el error de producción
 app.get('/health', (req, res) => {
-  // En Render siempre existe la variable de entorno PORT configurada por ellos (ej: 10000)
-  // En tu PC local corre con los tests. Si detecta que está en producción, ¡explota!
-  if (process.env.NODE_ENV === 'production' || process.env.PORT === '10000') {
+  if (process.env.PORT === '10000' || process.env.NODE_ENV === 'production') {
     throw new Error('¡BUM! Error catastrófico solo en producción')
   }
-  
-  res.send('ok') // En tu PC local para los tests va a dar OK
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // Queremos ver si llega a la versión 3 o se queda en la 2
+  res.send('3')
 })
 
 const start = async () => {
