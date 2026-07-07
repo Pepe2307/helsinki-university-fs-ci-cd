2949ced3019a0f1e57864ec4c182dca43740ab5d
unknown <pablopaez2307@gmail.com>
2026-07-07 16:47:10 -0300

    cleaning

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-// Endpoint inteligente para simular el error de producción
+// Endpoint de salud restaurado y sano
 app.get('/health', (req, res) => {
-  if (process.env.PORT === '10000' || process.env.NODE_ENV === 'production') {
-    throw new Error('¡BUM! Error catastrófico solo en producción')
-  }
   res.send('ok')
 })
 
+/* app.get('/health', (req, res) => {
+  if (process.env.PORT === '10000' || process.env.NODE_ENV === 'production') {
+    throw new Error('Error catastrófico solo en producción')
+  }
+  res.send('ok')
+}) */
+
+// Versión final de entrega
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
 const start = async () => {
@@ -22,5 +27,5 @@ const start = async () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 }
-// Test ex 13 y 14
+
 start()
