6f8f57736fe61e45c3eb86bd084c9ed2be5a74c8
unknown <pablopaez2307@gmail.com>
2026-07-07 12:05:04 -0300

    health check

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-// NUEVO: Endpoint dummy para chequear la versión en producción
+// NUEVO: Endpoint para el control de salud de Render
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// Tu endpoint anterior de versión
 app.get('/version', (req, res) => {
   res.send('2')
 })
-
 const start = async () => {
   await app.listen(PORT)
   // eslint-disable-next-line no-console
